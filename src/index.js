63ee914dd84a5ae3714bad571c0aed671a816435
Yair Levi <lyair1@gmail.com>
2020-04-11 15:27:37 -0400

    Add 'move' method to move the Swipeable content using the Swipeable ref

--- src/index.js
+++ src/index.js
@@ -173,14 +173,12 @@ export default class Swipeable extends PureComponent {
     rightButtonsOpen: false
   };
 
-  componentWillMount() {
+  componentDidMount() {
     const {onPanAnimatedValueRef, onRef} = this.props;
 
     onRef(this);
     onPanAnimatedValueRef(this.state.pan);
-  }
 
-  componentDidMount() {
     if (this.props.bounceOnMount) {
       setTimeout(this._bounceOnMount, 700);
     }
@@ -189,14 +187,6 @@ export default class Swipeable extends PureComponent {
   componentWillUnmount() {
     this._unmounted = true;
   }
-
-  recenter = (
-    animationFn = this.props.swipeReleaseAnimationFn,
-    animationConfig = this.props.swipeReleaseAnimationConfig,
-    onDone
-  ) => {
-    move(animationFn, animationConfig, onDone)
-  };
 
   move = (
     animationFn = this.props.swipeReleaseAnimationFn,
@@ -220,6 +210,14 @@ export default class Swipeable extends PureComponent {
     pan.flattenOffset();
 
     animationFn(pan, animationConfig).start(onDone);
+  };
+
+  recenter = (
+    animationFn = this.props.swipeReleaseAnimationFn,
+    animationConfig = this.props.swipeReleaseAnimationConfig,
+    onDone
+  ) => {
+    this.move(animationFn, animationConfig, onDone)
   };
 
   _bounceOnMount = () => {
